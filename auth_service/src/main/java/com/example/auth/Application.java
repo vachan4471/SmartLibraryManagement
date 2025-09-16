71ab6d39f2628ee3b88859ba9618521ffe4ab49c
vachan4471 <vachanhanamanal4471@gmail.com>
2025-09-16 14:21:54 +0530

    added Dotenv dependency to load env variables

--- auth_service/src/main/java/com/example/auth/Application.java
+++ auth_service/src/main/java/com/example/auth/Application.java
@@ -4,6 +4,10 @@ import io.micronaut.runtime.Micronaut;
 
 public class Application {
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.load();//this will be HashMap.
+        dotenv.entries().forEach(entry ->
+                System.setProperty(entry.getKey(), entry.getValue())
+        );
         Micronaut.run(Application.class, args);
     }
 }
